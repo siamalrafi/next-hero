22efd4f31c402fa3acd59339809c55e6ae1cc514
siamalrafi <rahathasandipto@gmail.com>
2023-07-31 16:37:35 +0600

    dashboard

--- src/app/(With_Layout)/about/page.js
+++ src/app/(With_Layout)/about/page.js
@@ -7,6 +7,7 @@ const page = () => {
    return (
       <div>
          <h1>This is the about page.</h1>
+         <h1>This is the about page.</h1>
       </div>
    );
 };
--- src/app/(With_Layout)/contact/page.js
+++ src/app/(With_Layout)/contact/page.js
@@ -7,6 +7,7 @@ const contact = () => {
    return (
       <div>
          <h1>this is the contact page.</h1>
+         <h1>this is the contact page.</h1>
       </div>
    );
 };
